236d481e7fd777bd83e4097004eb902621c52e75
Sonal Gholap <sonal.gholap@spit.ac.in>
2024-12-03 20:11:12 +0530

    added multiple ways of testing fields

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ using System;
 
 class CloudQATest 
 {
+    static IWebElement FindElementMultipleWays(IWebDriver driver, params By[] locators){
+        foreach(By locator in locators){
+            try{
+                var elements = driver.FindElements(locator);
+                if(elements.Count > 0){
+                    return elements[0];
+                }
+            }
+            catch { 
+
+            }
+        }
+
+        throw new Exception("Could not find element");
+    }
+
     static void Main(string[] args)
     {
         IWebDriver driver = new ChromeDriver();
@@ -14,8 +30,16 @@ class CloudQATest
             driver.Navigate().GoToUrl("https://app.cloudqa.io/home/AutomationPracticeForm");
 
             // Field 1: First Name Input
-            var firstNameInput = driver.FindElement(By.Id("fname"));
+            var firstNameInput = FindElementMultipleWays(driver, 
+                By.CssSelector(".form-group input[type='text'][name='First Name']"),
+                By.Id("fname"),
+                By.Name("First Name"),
+                By.XPath("//input[@placeholder='Name']")
+            );
+
+            firstNameInput.Clear();
             firstNameInput.SendKeys("Govind");
+
             string enteredName = firstNameInput.GetDomProperty("value");
             
             if(enteredName == "Govind")
@@ -25,7 +49,13 @@ class CloudQATest
 
 
             // Field 2: Gender Select
-            var genderMale = driver.FindElement(By.Id("male"));
+            var genderMale = FindElementMultipleWays(driver,
+                By.CssSelector("input[type='radio'][value='Male']"),
+                By.Id("male"),
+                By.Name("gender"),
+                By.XPath("//input[@value='Male']")
+            );
+
             genderMale.Click();
 
             if(genderMale.Selected)
@@ -33,28 +63,38 @@ class CloudQATest
             else
                 throw new Exception("Gender test failed!");
 
-            // Field 3: Country Input
-            var countryInput = driver.FindElement(By.Id("country"));
-            countryInput.SendKeys("India");
 
-            var countrySuggestion = driver.FindElement(
-                By.XPath("//div[@id='countryautocomplete-list']//div[contains(strong, 'India')]")
+            // Field 3: Country Input
+            var countryInput = FindElementMultipleWays(driver,
+                By.CssSelector(".autocomplete input[type='text'][placeholder='Country']"),
+                By.Id("country"),
+                By.Name("Country"),
+                By.XPath("//input[@placeholder='Country']")
             );
+
+            countryInput.Clear();
+            countryInput.SendKeys("United");
+
+            var countrySuggestion = FindElementMultipleWays(driver,
+                By.CssSelector(".autocomplete-items div input[type='hidden'][value='United Kingdom']/.."),
+                By.XPath("//input[@type='hidden' and @value='United Kingdom']/..")
+                // By.CssSelector(".autocomplete-items div input[type='hidden'][value='United Kingdom']")
+                // By.XPath("//div[@id='countryautocomplete-list']//div[contains(strong, 'United Kingdom')]")
+            );
+
             countrySuggestion.Click();
 
             string selectedCountry = countryInput.GetDomProperty("value");
             
-            if(selectedCountry == "India")
+            if(selectedCountry == "United Kingdom")
                 Console.WriteLine("Country autocomplete test passed!");
             else
                 throw new Exception("Country autocomplete test failed!");
         }
-        catch (Exception ex)
-        {
+        catch(Exception ex){
             Console.WriteLine($"Test failed: {ex.Message}");
         }
-        finally 
-        {
+        finally {
             driver.Quit();
         }
     }
